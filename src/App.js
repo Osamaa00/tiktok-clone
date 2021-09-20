4acce5a5644092fa21714b321498f464da51fed1
Osama <osamanadeem679@gmail.com>
2021-09-20 12:06:42 +0500

    built Video and Video footer component to play/pause the video and show other features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import Video from './Video/Video';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Tiktok Clone</h1>
+
+      <div className="app__videos">
+        {/* video components here */}
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
